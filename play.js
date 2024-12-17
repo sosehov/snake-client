4d08b5e844bc0d1c3926f25ff298f8bf12269fad
Soseh <sosehv@gmail.com>
2024-12-17 16:14:15 -0500

    Removed connect function and added require connect instead

--- play.js
+++ play.js
@@ -1,20 +1,4 @@
-const net = require("net");
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: '10.0.0.182',
-    port: 50541,
-  });
+const connect = require("./client");
+  
 
-  conn.on("data", () => {
-    console.log("Server says: ", data);
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
-
-console.log("Connecting...")
-connect();
