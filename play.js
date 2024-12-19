f89de204cc386f80b4f3f84cc79cac26e58fa14c
Soseh <sosehv@gmail.com>
2024-12-19 11:39:49 -0500

    adds a call to process.exit function and removes const con that wasn't used

--- play.js
+++ play.js
@@ -1,10 +1,7 @@
 const connect = require("./client");
 
-console.log("Connecting...")
-const con = connect();
-
 // setup interface to handle user input from stdins
-const setupInput = function () {
+const setupInput = function() {
   const stdin = process.stdin;  // create variable to hold the stdin object so we don't have to type process.stdin multiple times
   stdin.setRawMode(true); // Raw Mode allows us to listen for individual keypresses instead of waiting for the user to press enter
   stdin.setEncoding("utf8"); // utf8 encoding is set so that we can read the text data that is input
@@ -13,10 +10,10 @@ const setupInput = function () {
   return stdin;   // return the stdin object so we can use it elsewhere in the program
 };
 
-const handleUserInput = function (key) {
-   if (key === "\u0003") {
-    process.exit
-   }
+const handleUserInput = function(key) {
+  if (key === "\u0003") {
+    process.exit();
+  }
 };
 
 connect();
