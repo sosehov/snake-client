cd98c17f39f3c6a2d59c444da573650f3062fe88
Soseh <sosehv@gmail.com>
2024-12-18 22:19:18 -0500

    Isolates the config and creates a new connection

--- client.js
+++ client.js
@@ -1,25 +1,28 @@
 const net = require("net");
-  
+
+const config = {
+  host: '10.0.0.182',
+  port: 50541,
+};
+
+ // Create a new connection 
 const connect = function() {
-  const conn = net.createConnection({
-    host: '10.0.0.182',
-    port: 50541,
-  });
-
- // conn.on("data", () => {
-    //console.log("Server says: ", data);
-  //});
-
+  const conn = net.createConnection(config);
+  
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
-  });
-
-  conn.on("connect", () => {
     conn.write("Name: SOS");
-  })
 
-// interpret incoming data as text
-conn.setEncoding("utf8");
+    // Send move command right after connecting
+    // conn.write("Move: up");
+  });
+  // Listening for incoming data. Allows us to understand what information we're receiving from the server after sending the Move: up command
+   conn.on("data", (data) => {
+      console.log("Received", data);
+    });
+
+    // interpret incoming data as text
+    conn.setEncoding("utf8");
 
 return conn;
 };
