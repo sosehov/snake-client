5abd4b7d579e0f1a9d77aee521239cae11bcf83c
Soseh <sosehv@gmail.com>
2024-12-23 14:41:35 -0500

    Adds movements keys and initials

--- constants.js
+++ constants.js
@@ -1,7 +1,20 @@
 const IP = '10.0.0.182';
 const PORT = 50541;
 
+const MOVE_UP_KEY = "w";
+const MOVE_LEFT_KEY = "a";
+const MOVE_DOWN_KEY = "s";
+const MOVE_RIGHT_KEY = "d";
+
+const initials = "SOS";
+
+// Export all the constants being used in the app
 module.exports = {
   IP,
   PORT,
+  MOVE_UP_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_RIGHT_KEY,
+  initials,
 };
