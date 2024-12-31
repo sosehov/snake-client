6d793a27eba8b360e486ffe53ff4a92efb667007
Soseh <sosehv@gmail.com>
2024-12-31 14:43:27 -0500

    Adds Say: to keyMessageMapping

--- constants.js
+++ constants.js
@@ -1,20 +1,27 @@
-const IP = '10.0.0.182';
-const PORT = 50541;
+const config = {
+  host: '10.0.0.182',
+  port: 50541
+}
 
 const MOVE_UP_KEY = "w";
 const MOVE_LEFT_KEY = "a";
 const MOVE_DOWN_KEY = "s";
 const MOVE_RIGHT_KEY = "d";
 
-const initials = "SOS";
+const initials = "Name: SOS";
+
+const keyMessageMapping = {
+  g: "Say: Viva Snake!",
+  l: "Say: Great game!",
+}
 
 // Export all the constants being used in the app
 module.exports = {
-  IP,
-  PORT,
+  config,
   MOVE_UP_KEY,
   MOVE_LEFT_KEY,
   MOVE_DOWN_KEY,
   MOVE_RIGHT_KEY,
   initials,
+  keyMessageMapping,
 };
